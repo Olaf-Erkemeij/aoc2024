59142420c3690f9104f07b2ebe761d68c260597f
Olaf-Erkemeij <o.a.erkemeij@gmail.com>
2024-12-13 14:29:42 +0100

    Solved day 8 and 9

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,13 +1,130 @@
+use std::collections::{HashMap, HashSet};
+use std::ops::{Add, Sub};
+
+use itertools::Itertools;
+
+type Input = (HashMap<char, Vec<Point>>, Point);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+
+    fn in_bound(&self, bound: &Point) -> bool {
+        (0..bound.x).contains(&self.x) && (0..bound.y).contains(&self.y)
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Input {
+    let bounds = Point::new(
+        input.lines().next().unwrap().len() as i64,
+        input.lines().count() as i64,
+    );
+
+    let antennas = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                (c != '.').then(|| (c, Point::new(x as i64, y as i64)))
+            })
+        })
+        .into_group_map();
+
+    (antennas, bounds)
+}
+
 pub fn solve_part1(input: &str) -> String {
     // Solve part 1
-    let _ = input;
-    String::from("Not implemented")
+    let (antennas, bounds) = parse_input(input);
+
+    antennas
+        .values()
+        .flat_map(|positions| {
+            positions
+                .iter()
+                .combinations(2)
+                .flat_map(|pair| {
+                    let antenna1 = *pair[0];
+                    let antenna2 = *pair[1];
+                    let diff = antenna2 - antenna1;
+
+                    vec![
+                        antenna2 + diff,
+                        antenna1 - diff
+                    ]
+                        .into_iter()
+                        .filter(|antinode| antinode.in_bound(&bounds))
+                })
+        })
+        .collect::<HashSet<_>>()
+        .len()
+        .to_string()
 }
 
 pub fn solve_part2(input: &str) -> String {
-    // Solve part 2
-    let _ = input;
-    String::from("Not implemented")
+    let (antennas, bounds) = parse_input(input);
+
+    antennas
+        .values()
+        .flat_map(|positions| {
+            positions
+                .iter()
+                .combinations(2)
+                .flat_map(|pair| {
+                    let antenna1 = *pair[0];
+                    let antenna2 = *pair[1];
+                    let diff = antenna2 - antenna1;
+
+                    let mut antinodes = vec![];
+                    let mut antinode = antenna2;
+
+                    while antinode.in_bound(&bounds) {
+                        antinodes.push(antinode);
+                        antinode = antinode + diff;
+                    }
+
+                    let mut antinode = antenna1;
+
+                    while antinode.in_bound(&bounds) {
+                        antinodes.push(antinode);
+                        antinode = antinode - diff;
+                    }
+
+                    antinodes
+                })
+        })
+        .collect::<HashSet<_>>()
+        .len()
+        .to_string()
 }
 
 pub fn solve(input: &str) {
--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,13 +1,56 @@
+type Input = (Vec<Vec<usize>>, Vec<Vec<usize>>);
+
+use itertools::{Either, Itertools};
+
+fn parse_input(input: &str) -> Input {
+    let mut p = 0;
+    // Get every digit that is in the input (it is one line)
+    input
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .enumerate()
+        .partition_map(|(i, n)| {
+            let res = (p as usize..p+n as usize).collect::<Vec<usize>>();
+            p += n as usize;
+            if i % 2 == 0 {
+                Either::Left(res)
+            } else {
+                Either::Right(res)
+            }
+        })
+        .into()
+}
+
 pub fn solve_part1(input: &str) -> String {
-    // Solve part 1
-    let _ = input;
-    String::from("Not implemented")
+    let (mut lhs, rhs) = parse_input(input);
+
+    let mut rhs = rhs.iter().flatten().collect::<Vec<_>>();
+
+    for y in (0..lhs.len()).rev() {
+        for x in (0..lhs[y].len()).rev() {
+            if lhs[y][x] >= *rhs[0] {
+                lhs[y][x] = *rhs[0];
+                rhs = rhs[1..].to_vec();
+            }
+        }
+    }
+
+    lhs.iter().enumerate().map(|(i, f)| f.iter().map(|j| j * i).sum::<usize>()).sum::<usize>().to_string()
 }
 
 pub fn solve_part2(input: &str) -> String {
-    // Solve part 2
-    let _ = input;
-    String::from("Not implemented")
+    let (mut lhs, mut rhs) = parse_input(input);
+
+    for y in (0..lhs.len()).rev() {
+        for x in 0..rhs.len() {
+            if rhs[x].len() >= lhs[y].len() && lhs[y][0] >= rhs[x][0] {
+                lhs[y] = rhs[x][..lhs[y].len()].to_vec();
+                rhs[x] = rhs[x][lhs[y].len()..].to_vec();
+            }
+        }
+    }
+
+    lhs.iter().enumerate().map(|(i, f)| f.iter().map(|j| j * i).sum::<usize>()).sum::<usize>().to_string()
 }
 
 pub fn solve(input: &str) {
